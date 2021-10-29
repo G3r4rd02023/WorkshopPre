3687a07e286c62780e15e80f2fbe0bdf7c9c62d0
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-10-29 14:08:31 -0600

    DocumentTypes

---
+++ WorkshopPre/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkshopPre.Data.Entities;
+
+namespace WorkshopPre.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
+            
+        }
+    }
+}
